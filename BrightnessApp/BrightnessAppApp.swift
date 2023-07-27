17d3f188aab93d924476caac825037e6006ed1ea
LAV-cloud <ZombieManiak172@outlook.com>
2023-07-27 20:28:45 +0600

    Initial Commit

---
+++ BrightnessApp/BrightnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrightnessAppApp.swift
+//  BrightnessApp
+//
+//  Created by Ромка Бережной on 27.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct BrightnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
